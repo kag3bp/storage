0859f4c7f0cb5b5a0a805009a4c1643f8b8ed076
Kalman Gabor (XC-AS/EPP2-Bp) <Gabor.Kalman2@hu.bosch.com>
2024-02-09 13:51:25 +0100

    add demonstration

--- storage/storage.cpp
+++ storage/storage.cpp
@@ -1,65 +1,120 @@
 #include <iostream>
 #include "list.hpp"
 
+class CObject
+{
+  public:
+    int number;
+};
+
 int main()
 {
-  CList<int, 8> l_list;
+  CList<int, 8> l_listInt;
+  CList<float, 8> l_listFloat;
+  CList<CObject, 3> l_listClass;
+
   unsigned int l_iterator;
-  int l_object;
 
-  std::cout << "\nAdd four numbers:\n";
+  //-------------------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------------------
+  std::cout << "\n------------------------------\n";
+  std::cout << "Add four integers:\n";
 
-  l_list.insert(7, 0);
-  l_list.insert(5, 0);
-  l_list.insert(3, 0);
-  l_list.insert(1, 0);
+  int l_int;
+
+  l_listInt.insert(7, 0);
+  l_listInt.insert(5, 0);
+  l_listInt.insert(3, 0);
+  l_listInt.insert(1, 0);
 
   l_iterator = 0;
-  while(l_list.get(l_iterator, l_object))
+  while(l_listInt.get(l_iterator, l_int))
   {
-    std::cout << "Object number: " << l_iterator << " Value: " << l_object << "\n";
-
+    std::cout << "Object number: " << l_iterator << " Value: " << l_int << "\n";
     l_iterator++;
   }
 
-  std::cout << "\nInsert the missing numbers:\n";
 
-  l_list.insert(0, 0);
-  l_list.insert(2, 2);
-  l_list.insert(4, 4);
-  l_list.insert(6, 6);
+  std::cout << "\nInsert the missing integers:\n";
+
+  l_listInt.insert(0, 0);
+  l_listInt.insert(2, 2);
+  l_listInt.insert(4, 4);
+  l_listInt.insert(6, 6);
 
   l_iterator = 0;
-  while(l_list.get(l_iterator, l_object))
+  while(l_listInt.get(l_iterator, l_int))
   {
-    std::cout << "Object number: " << l_iterator << " Value: " << l_object << "\n";
-
+    std::cout << "Object number: " << l_iterator << " Value: " << l_int << "\n";
     l_iterator++;
   }
 
-  std::cout << "\nRemove firstly added four numbers:\n";
+  //save list state for further presentation
+  std::cout << "\nRemove firstly added four integers:\n\n";
 
-  l_list.remove(1);
-  l_list.remove(2);
-  l_list.remove(3);
-  l_list.remove(4);
+  l_listInt.remove(1);
+  l_listInt.remove(2);
+  l_listInt.remove(3);
+  l_listInt.remove(4);
 
   l_iterator = 0;
-  while(l_list.get(l_iterator, l_object))
+  while(l_listInt.get(l_iterator, l_int))
   {
-    std::cout << "Object number: " << l_iterator << " Value: " << l_object << "\n";
+    std::cout << "Object number: " << l_iterator << " Value: " << l_int << "\n";
+    l_iterator++;
+  }
 
+  //-------------------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------------------
+  std::cout << "\n------------------------------\n";
+  std::cout << "Add two floats:\n";
+
+  float l_float;
+
+  l_listFloat.insert(2.F, 0);
+  l_listFloat.insert(1.F, 0);
+
+  l_iterator = 0;
+  while(l_listFloat.get(l_iterator, l_float))
+  {
+    std::cout << "Object number: " << l_iterator << " Value: " << l_float << "\n";
+    l_iterator++;
+  }
+
+  std::cout << "\nAdd three floats in the middle of the two:\n";
+
+  l_listFloat.insert(1.25F, 1);
+  l_listFloat.insert(1.5F,  2);
+  l_listFloat.insert(1.75F, 3);
+
+  l_iterator = 0;
+  while(l_listFloat.get(l_iterator, l_float))
+  {
+    std::cout << "Object number: " << l_iterator << " Value: " << l_float << "\n";
+    l_iterator++;
+  }
+
+  //-------------------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------------------
+  std::cout << "\n------------------------------\n";
+  std::cout << "Add three classes:\n";
+
+  CObject l_object;
+
+  l_object.number = 1;
+  l_listClass.insert(l_object, 0);
+  l_object.number = 2;
+  l_listClass.insert(l_object, 1);
+  l_object.number = 3;
+  l_listClass.insert(l_object, 2);
+
+  l_iterator = 0;
+  while(l_listClass.get(l_iterator, l_object))
+  {
+    std::cout << "Object number: " << l_iterator << " Value: " << l_object.number << "\n";
     l_iterator++;
   }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
